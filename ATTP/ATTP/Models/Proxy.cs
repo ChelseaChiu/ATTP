8d60030a05c849d79d47b5bbe106fbb68d1b3d30
Chelsea Chiu <chelseachiu.happy@gmail.com>
2019-11-01 12:07:32 +1030

    fixed the proxy state

--- ATTP/ATTP/Models/Proxy.cs
+++ ATTP/ATTP/Models/Proxy.cs
@@ -53,27 +53,45 @@ namespace ATTP.Models
             try
             {
                 proxy = new StudentServiceClient(StudentServiceClient.EndpointConfiguration.BasicHttpsBinding_IStudentService);
-                User user = new User();
-                var s = proxy.GetStudentById(id);
-
-
-                if (s != null)
+                if (proxy != null)
                 {
-                    user.Username = String.Format(s.GivenName + "  " + s.LastName);
-                    user.Id = s.StudentID;
-                    proxy.CloseAsync();
-                    return user;
-               }
+                    if (proxy.State != CommunicationState.Faulted)
+                    {
+                        proxy = new StudentServiceClient(StudentServiceClient.EndpointConfiguration.BasicHttpsBinding_IStudentService);
+                        User user = new User();
+                        var s = proxy.GetStudentById(id);
+                        if (s != null)
+                        {
+                            user.Username = String.Format(s.GivenName + "  " + s.LastName);
+                            user.Id = s.StudentID;
+                            proxy.CloseAsync();
+                            return user;
+                        }
+                        else
+                        {
+                            proxy.CloseAsync();
+                            return null;
+                        }
+                    }
+                    else
+                    {
+                        proxy.Abort();
+                        return null;
+                    }
+
+                }
                 else
                 {
-                    proxy.CloseAsync();
+                    proxy.Abort();
                     return null;
                 }
+
+
             }
             catch (CommunicationException)
             {
                 proxy.Abort();
-                return null ;
+                return null;
             }
             catch (TimeoutException)
             {
@@ -94,24 +112,43 @@ namespace ATTP.Models
             try
             {
                 proxy = new StudentServiceClient(StudentServiceClient.EndpointConfiguration.BasicHttpsBinding_IStudentService);
-                List<Qualification> qualifications = new List<Qualification>();
-                var qList = proxy.GetQualificationList(studentId);
-                for (int i = 0; i < qList.Count; i++)
+                if (proxy!=null)
                 {
-                    Qualification qual = new Qualification();
-                    qual.QualCode = qList[i].QualCode;
-                    qual.QualName = qList[i].QualName;
-                    qual.CoreUnits = qList[i].CoreUnits;
-                    qual.TafeQualCode = qList[i].TafeQualCode;
-                    qual.NationaQualCode = qList[i].NationalQualCode;
-                    qual.TotalUnits = qList[i].TotalUnits;
-                    qual.ElectedUnits = qList[i].ElectedUnits;
-                    qual.ReqListedElectedUnits = qList[i].ReqListedElectedUnits;
+                    if (proxy.State!=CommunicationState.Faulted)
+                    {
+                        List<Qualification> qualifications = new List<Qualification>();
+                        var qList = proxy.GetQualificationList(studentId);
+                        for (int i = 0; i < qList.Count; i++)
+                        {
+                            Qualification qual = new Qualification();
+                            qual.QualCode = qList[i].QualCode;
+                            qual.QualName = qList[i].QualName;
+                            qual.CoreUnits = qList[i].CoreUnits;
+                            qual.TafeQualCode = qList[i].TafeQualCode;
+                            qual.NationaQualCode = qList[i].NationalQualCode;
+                            qual.TotalUnits = qList[i].TotalUnits;
+                            qual.ElectedUnits = qList[i].ElectedUnits;
+                            qual.ReqListedElectedUnits = qList[i].ReqListedElectedUnits;
 
-                    qualifications.Add(qual);
+                            qualifications.Add(qual);
+                        }
+                        proxy.CloseAsync();
+                        return qualifications;
+                    }
+                    else
+                    {
+                        proxy.Abort();
+                        return null;
+                    }
+                    
                 }
-                proxy.CloseAsync();
-                return qualifications;
+                else
+                {
+                    proxy.Abort();
+                    return null;
+                }
+
+
             }
             catch (CommunicationException)
             {
@@ -135,18 +172,38 @@ namespace ATTP.Models
             try
             {
                 proxy = new StudentServiceClient(StudentServiceClient.EndpointConfiguration.BasicHttpsBinding_IStudentService);
-                double progress = 0.00d;
-                progress = proxy.CalQualProgress(studentId, qualCode);
-                if (progress >= 1)
+                if (proxy != null)
                 {
-                    proxy.CloseAsync();
-                    return 1.00d;
+                    if (proxy.State != CommunicationState.Faulted)
+                    {
+                        proxy = new StudentServiceClient(StudentServiceClient.EndpointConfiguration.BasicHttpsBinding_IStudentService);
+                        double progress = 0.00d;
+                        progress = proxy.CalQualProgress(studentId, qualCode);
+                        if (progress >= 1)
+                        {
+                         proxy.CloseAsync();
+                            return 1.00d;
+                        }
+                        else
+                        {
+                            proxy.CloseAsync();
+                            return progress;
+                        }
+                    }
+                    else
+                    {
+                        proxy.Abort();
+                        return 0;
+                    }
+
                 }
                 else
                 {
-                    proxy.CloseAsync();
-                    return progress; 
+                    proxy.Abort();
+                    return 0;
                 }
+
+
             }
             catch (CommunicationException)
             {
@@ -166,28 +223,49 @@ namespace ATTP.Models
 
         }
 
+
+
         public static List<Competency> GetCompetencies(string studentId, string qualCode)
         {
-
             try
             {
                 proxy = new StudentServiceClient(StudentServiceClient.EndpointConfiguration.BasicHttpsBinding_IStudentService);
-                List<Competency> competencies = new List<Competency>();
-                var cList = proxy.GetCompetencyList(studentId, qualCode);
-                for (int i = 0; i < cList.Count; i++)
+                if (proxy != null)
                 {
-                    Competency comp = new Competency();
-                    comp.TafeCompCode = cList[i].TafeCode;
-                    comp.NationaCompCode = cList[i].NationalCode;
-                    comp.CompetencyName = cList[i].CompetencyName;
-                    comp.SubjectCode = cList[i].SubjectCode;
-                    comp.CompTypeCode = cList[i].TrainingPakckageUsage;
-                    comp.Results = cList[i].Results;
-                    competencies.Add(comp);
+                    if (proxy.State != CommunicationState.Faulted)
+                    {
+                        proxy = new StudentServiceClient(StudentServiceClient.EndpointConfiguration.BasicHttpsBinding_IStudentService);
+                        List<Competency> competencies = new List<Competency>();
+                        var cList = proxy.GetCompetencyList(studentId, qualCode);
+                        for (int i = 0; i < cList.Count; i++)
+                        {
+                            Competency comp = new Competency();
+                            comp.TafeCompCode = cList[i].TafeCode;
+                            comp.NationaCompCode = cList[i].NationalCode;
+                            comp.CompetencyName = cList[i].CompetencyName;
+                            comp.SubjectCode = cList[i].SubjectCode;
+                            comp.CompTypeCode = cList[i].TrainingPakckageUsage;
+                            comp.Results = cList[i].Results;
+                            competencies.Add(comp);
+
+                        }
+                        proxy.CloseAsync();
+                        return competencies;
+                    }
+                    else
+                    {
+                        proxy.Abort();
+                        return null;
+                    }
 
                 }
-                proxy.CloseAsync();
-                return competencies;
+                else
+                {
+                    proxy.Abort();
+                    return null;
+                }
+
+
             }
             catch (CommunicationException)
             {
@@ -204,6 +282,7 @@ namespace ATTP.Models
 
                 throw;
             }
+
         }
 
 
