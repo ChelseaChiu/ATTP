345d2a72ac52ee48f0962ce248c2ac9e059e6723
Heather L <lysna87@gmail.com>
2020-04-02 10:52:27 +1030

    Implemented another campus and picker to choose from

--- ATTP/ATTP/Views/CampusDirectory.xaml.cs
+++ ATTP/ATTP/Views/CampusDirectory.xaml.cs
@@ -20,45 +20,66 @@ namespace ATTP.Views
         public List<String> FloorList = new List<string>() { "Currie St Basement" }; //TODO change this to use models.
         public String floor1 = "Currie St First Floor";
 
-        //Model setup
+        public List<String> CampusList = new List<string>() {  "Currie St" , "Regency" };
+
+        //Picker using Model setup
         public ObservableCollection<Floor> Floors { get; set; }
+
+        public ObservableCollection<Floor> RegencyFloors { get; set; }
+        public ObservableCollection<Floor> CurrieFloors { get; set; }
         public CampusDirectory()
         {
             InitializeComponent();
-            //initial image change test
+            //initial image change test from default image
             imgURL = "Assets/CampusImages/SouthAustralia/CurrieStBasementFloor.png";
             //set up initial picker content (Comment this out when models implemented)
             FloorList.Add(floor1);
 
-            //using model
+            //using model. These are the default floors (Adelaide campus)
             Floors = new ObservableCollection<Floor> { new Floor { FloorName = "Basement", FloorImgUrl = "Assets/CampusImages/SouthAustralia/CurrieStBasementFloor.png" } };
-            //Floors.Add(new Floor { );
+
+            
+            Floors.Add(new Floor { FloorName = "Ground Floor", FloorImgUrl = "Assets/CampusImages/SouthAustralia/CurrieStGroundFloor.png" });
             Floors.Add(new Floor { FloorName = "First Floor", FloorImgUrl = "Assets/CampusImages/SouthAustralia/CurrieStFirstFloor.png" });
             Floors.Add(new Floor { FloorName = "Second Floor", FloorImgUrl = "Assets/CampusImages/SouthAustralia/CurrieStSecondFloor.png" });
             Floors.Add(new Floor { FloorName = "Third Floor", FloorImgUrl = "Assets/CampusImages/SouthAustralia/CurrieStThirdFloor.png" });
             Floors.Add(new Floor { FloorName = "Fourth Floor", FloorImgUrl = "Assets/CampusImages/SouthAustralia/CurrieStFourthFloor.png" });
 
+            RegencyFloors = new ObservableCollection<Floor> { new Floor { FloorName = "Ground Floor", FloorImgUrl = "Assets/CampusImages/SouthAustralia/RegencyGroundFloor.png" } };
+            RegencyFloors.Add(new Floor { FloorName = "First Floor", FloorImgUrl = "Assets/CampusImages/SouthAustralia/RegencyFirstFloor.png" });
+            CurrieFloors = Floors; //currie list is the default floors list.
 
             //the picker that chooses the floor. TODO: have default floors be Currie St.
-            //imagePicker.ItemsSource = FloorList;
+            //imagePicker.ItemsSource = FloorList; //old method
 
             imagePicker.ItemsSource = Floors;
+
+            campusPicker.ItemsSource = CampusList;
+            campusPicker.SelectedItem = CampusList[0];
 
             BindingContext = this;
 
 
         }
 
+
+
+
         //picks the floor
         private void imagePicker_SelectedIndexChanged(object sender, EventArgs e)
         {
             //code to handle specific image picked. Old way.
-            var selection = imagePicker.SelectedIndex;
+            //var selection = imagePicker.SelectedIndex;
+
             var floorSelection = imagePicker.SelectedItem as Floor;
 
             string newImg = imgURL; //default image change
-
-            newImg = floorSelection.FloorImgUrl;
+            if(floorSelection != null)
+            {
+                newImg = floorSelection.FloorImgUrl;
+                floorImg.Source = newImg;
+            }
+                
             /*
              *
             //Hard coded method
@@ -85,17 +106,48 @@ namespace ATTP.Views
             */
 
 
-            floorImg.Source = newImg;
+            
            // floorImg.App
+        }
+
+        private void campusPicker_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            var campusSelection = campusPicker.SelectedItem as string;
+            //may as well leave this hard coded
+            switch (campusSelection)
+            {
+                case "Currie St":
+                    {
+                        imagePicker.ItemsSource = CurrieFloors;
+                        //imagePicker.SelectedItem = CurrieFloors.FirstOrDefault<Floor>;
+                        imagePicker.SelectedItem = CurrieFloors[0];
+                        //default floor image here
+                        //
+                        break;
+                    }
+                case "Regency":
+                    {
+                        imagePicker.ItemsSource = RegencyFloors;
+                        //default floor image here
+                        imagePicker.SelectedItem = RegencyFloors[0];
+                        break;
+                    }
+                default:
+                    {
+                        imagePicker.ItemsSource = Floors; //default floors are adelaide campus
+                        break;
+                    }
+            }
+
+
         }
 
         //public ObservableCollection<CrnSessionTimetable> Classes { get; set; }
 
 
-        /*TODO: set up class for locations
-         *TODO: set up initial picker to show a small group of images for testing
-         *TODO: set up image to show default map
-         *TODO: set up picker selection changed method to show image based on selection
+        /*
+         *TODO: 
+         *TODO: set up picker selection changed method to show image based on selection DONE
          *TODO once working, update picker to handle all AU tafe maps. (Can use 2 pickers).
          * 
          */
